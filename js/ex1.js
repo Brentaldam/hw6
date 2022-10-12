0a41ee2bb0b573c3ab81966bf723b65e389f1c8c
Brentaldam <brent.marriott@student.csulb.edu>
2022-10-11 21:03:56 -0700

    version1.1

---
+++ js/ex1.js
@@ -0,0 +1,8 @@
+const bodyNodes = () => {
+   return document.body.querySelectorAll('*');
+}
+
+const nodes = bodyNodes();
+
+
+console.log(nodes);
---
+++ js/ex2.js
@@ -0,0 +1,7 @@
+//Complete code for the following function
+//Display text for 400 level courses
+const displayText400 = (el) => {
+    return document.body.getElementsByClassName(el);
+}
+const dis = displayText400('400');
+console.log(dis);
